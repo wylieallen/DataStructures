3f9314443c21f75aa438ef6307f4ee564c111509
wylieallen <wylieallen1@gmail.com>
2018-06-07 23:05:35 -0400

    Added exception throwing to a couple methods in the List interface

--- src/datastructs/lists/List.java
+++ src/datastructs/lists/List.java
@@ -4,8 +4,8 @@ public interface List<T>
 {
     T getHead();
     T getTail();
-    T get(int index);
-    T remove(int index);
+    T get(int index) throws IndexOutOfBoundsException;
+    T remove(int index) throws IndexOutOfBoundsException;
     void remove(T value);
     void append(T value);
     void prepend(T value);
